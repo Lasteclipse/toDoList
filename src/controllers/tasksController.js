98b09caaa050b8d99c65df96ffb25a3eb4c83d96
Kevin <kevin_allan2000@outlook.com>
2023-09-18 14:33:24 -0300

    First commit

---
+++ src/controllers/tasksController.js
@@ -0,0 +1,36 @@
+const taskModel = require('../models/tasksModel');
+
+const getAll =  async (_request, response) => {
+
+    const tasks = await taskModel.getAll();  
+    return response.status(200).json(tasks);
+};
+
+const createdTask = async (request, response) => {
+
+    const createdTask = await taskModel.createTask(request.body);
+
+    return response.status(201).json(createdTask);
+};
+
+const deleteTask = async (request, response) => {
+    const { id } = request.params;
+
+    await taskModel.deleteTask(id);
+    return response.status(204).json();
+};
+
+const updateTask = async (request, response) => {
+    const { id } = request.params;
+    await taskModel.updateTask(id, request.body);
+
+    return response.status(204).json();
+};
+
+
+module.exports = {
+    getAll,
+    createdTask,
+    deleteTask,
+    updateTask
+};
---
+++ src/router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const taskController = require('./controllers/tasksController');
+
+const router = express.Router();
+const TasksMiddleware = require('./middlewares/TasksMiddleware');
+
+router.get('/tasks', taskController.getAll);
+router.post('/tasks', TasksMiddleware.validateFieldTitle, taskController.createdTask);
+router.delete('/tasks/:id', taskController.deleteTask);
+router.put('/tasks/:id', TasksMiddleware.validateFieldStatus, TasksMiddleware.validateFieldTitle, taskController.updateTask);
+
+
+module.exports = router;
